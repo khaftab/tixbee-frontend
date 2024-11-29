373c3c7737d2ed0600326ba601c7c7df9ca163f0
khaftab <kh7aftab8uddin6ahmed@gmail.com>
2024-11-30 00:47:34 +0530

    add shadcn - add necessary config in tailwind.config.ts - add custom fonts

---
+++ app/routes/haka.tsx
@@ -0,0 +1,11 @@
+import { Button } from "~/components/ui/button";
+
+export default function Haka() {
+  return (
+    <div>
+      <h1 className="font-bold bg-red-400">Haka</h1>
+      <p className="font-inter-medium">This is the Haka page.</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
